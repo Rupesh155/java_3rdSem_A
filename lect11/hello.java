16f050b2d4bb70e8072a9e2fa4a695dd452e19f5
Rupesh155 <rudhg35@gmail.com>
2025-11-20 13:17:13 +0530

    code

--- lect11/hello.java
+++ lect11/hello.java
@@ -67,46 +67,47 @@ package lect11;
 //         System.out.println("C show()");
 //     }
 // }
- abstract  class Shape{
-    abstract  void  area();
 
-}
-class circle extends Shape{
-    int r;
+//  abstract  class Shape{
+//     abstract  void  area();
 
-    public circle(int r) {
-        this.r=r;
-    }
+// }
+// class circle extends Shape{
+//     int r;
+
+//     public circle(int r) {
+//         this.r=r;
+//     }
     
-    void area(){
-        double result=3.14*r*r;
-        System.out.println(result);
+//     void area(){
+//         double result=3.14*r*r;
+//         System.out.println(result);
 
-    }
+//     }
+// }           
+// class Rectangle extends  Shape{
+//     int length;
+//     int b;
+//     public Rectangle(int length,int b) {
+//         this.length=length;
+//         this.b=b;
+//     }
+//     void area(){
+        
+//         double result=length*b;
+//         System.out.println(result);
+        
+//     }
 
-}
-class Rectangle extends  Shape{
-    int length;
-    int b;
-    public Rectangle(int length,int b) {
-        this.length=length;
-        this.b=b;
-    }
-    void area(){
+// }
+// class hello{
+//     public static void main(String[] args) {
+//         circle s1=new circle(4);
+//         s1.area();
+//         Rectangle s2=new Rectangle(4, 5);
+//         s2.area();
         
-        double result=length*b;
-        System.out.println(result);
-        
-    }
+//     }
+// }
 
-}
-class hello{
-    public static void main(String[] args) {
-        circle s1=new circle(4);
-        s1.area();
-        Rectangle s2=new Rectangle(4, 5);
-        s2.area();
-        
-    }
-}
 
