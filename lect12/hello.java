0ee13631b7e04e1e2ecf0dc08cb73f10ec3276b1
Rupesh155 <rudhg35@gmail.com>
2025-11-21 16:16:55 +0530

    youTube

---
+++ lect12/hello.java
@@ -0,0 +1,142 @@
+package lect12;
+
+// class Bank{
+//    private  final  int TotalBalance=100;
+//     public   void getBalance(){
+//     System.out.println(TotalBalance);
+
+//    }
+//    public void setBalance(int amount){
+//     if(amount>0){
+//         TotalBalance+=amount;
+//     }
+
+//    }
+   
+// }
+
+// public class hello {
+//     public static void main(String[] args) {
+//         Bank s1=new Bank();
+//         s1.getBalance();
+//         s1.setBalance(5000);
+//         s1.getBalance();     
+//     }
+    
+// }
+
+// class Atm{
+//    private   int TotalBalance=1000000;
+//    public void getTotalB(){
+//     System.out.println(TotalBalance);
+//    }
+//    public  void setBalance(int amount){
+//     if(amount>0  && TotalBalance>=amount){
+//         TotalBalance-=amount;
+//         System.out.println(TotalBalance);
+//     }
+//    }
+// }
+
+// class hello{
+//     public static void main(String[] args) {
+//         Atm s1=new Atm();
+//         s1.getTotalB();
+//         s1.setBalance(2000);
+     
+        
+//     }
+// }
+
+// (new Random) 0-1
+// 0.5
+// 0.4*1000
+
+// final  int a=5;
+
+// class Atm{ 
+//     private  int TotalBalance=10000;
+//     public  void getB(){
+//         System.out.println(TotalBalance);
+//     }
+//     public  void setBalance(int amount){
+//         if(amount>0 ){
+//             TotalBalance+=amount;
+//         }
+
+//     }
+// }
+
+// class hello{
+//     public static void main(String[] args) {
+//         Atm s1=new Atm();
+//         s1.getB();
+//         s1.setBalance(400);
+        
+//     }
+// }
+
+class YouTube{
+   private   String ChannelName;
+  private   int    SubscriberCount;
+
+    public YouTube(String ChannelName) {
+        this.ChannelName=ChannelName;
+        this.SubscriberCount=0;
+    }
+    public  void getName(int pin){
+        if(pin==123){
+            System.out.println(ChannelName);
+
+        }
+     
+
+    }
+    public  void getSubscriberCount(){
+        System.out.println(SubscriberCount);
+     
+    }
+    public void setSubscriberCount(){
+        SubscriberCount++;
+
+    }
+    public void setunSubscriberCount(){
+        if(SubscriberCount>0){
+            SubscriberCount--;
+
+        }else{
+            System.out.println("ye sahi nhi hai ham aapko bata rhe hai..??");
+        }
+      
+
+    }
+
+  
+
+
+
+}
+class hello{
+    public static void main(String[] args) {
+        YouTube s1=new YouTube("globalJs");
+        s1.getSubscriberCount();
+        s1.setSubscriberCount();
+        s1.setSubscriberCount();
+        s1.setSubscriberCount();
+        s1.setSubscriberCount();
+        s1.getSubscriberCount();
+        s1.setunSubscriberCount();
+        s1.setunSubscriberCount();
+        s1.getSubscriberCount();
+
+
+        
+    }
+}
+
+
+
+
+
+
+
